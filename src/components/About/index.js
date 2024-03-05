2392efd16b4010a23c5a5602e6ed976b215d0875
Patrick Dunn <pmdunn78@gmail.com>
2024-03-05 12:03:17 -0500

    adjusting color scheme

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,25 +6,26 @@ const About = () => {
       <div className="img-wrapper">
         <img src={fb_headshot} alt="head and shoulders" />
       </div>
+      <div className="about-me-text-wrapper">
+        <p className="about-me-text">
+          I am a dedicated web developer with a passion for crafting robust and
+          efficient applications using the MERN (MongoDB, Express.js, React.js,
+          Node.js) stack. With several years of experience in the field, I have
+          honed my skills in creating dynamic and interactive web solutions that
+          meet the unique needs of each project.
+        </p>
 
-      <p className="about-me-text">
-        I am a dedicated web developer with a passion for crafting robust and
-        efficient applications using the MERN (MongoDB, Express.js, React.js,
-        Node.js) stack. With several years of experience in the field, I have
-        honed my skills in creating dynamic and interactive web solutions that
-        meet the unique needs of each project.
-      </p>
-
-      <p className="about-me-text">
-        My expertise in front-end development combined with back-end experience,
-        allows me to seamlessly integrate user-facing elements with server-side
-        logic. I thrive in collaborative environments, where I can leverage my
-        strong communication skills to work closely with clients and team
-        members to deliver high-quality results on time and within budget.
-        Whether I'm building a responsive user interface or optimizing database
-        performance, I approach each task with creativity, attention to detail,
-        and a commitment to excellence.
-      </p>
+        <p className="about-me-text">
+          My expertise in front-end development combined with back-end
+          experience, allows me to seamlessly integrate user-facing elements
+          with server-side logic. I thrive in collaborative environments, where
+          I can leverage my strong communication skills to work closely with
+          clients and team members to deliver high-quality results on time and
+          within budget. Whether I'm building a responsive user interface or
+          optimizing database performance, I approach each task with creativity,
+          attention to detail, and a commitment to excellence.
+        </p>
+      </div>
     </section>
   );
 };
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,11 @@
 const Header = (props) => {
   return (
-    <header className="header-container">
-      <h1 className="header-lg">Patrick Dunn</h1>
-      {props.children}
-    </header>
+    <div className="header-wrapper">
+      <header className="header-container">
+        <h1 className="header-lg">Patrick Dunn</h1>
+        {props.children}
+      </header>
+    </div>
   );
 };
 
