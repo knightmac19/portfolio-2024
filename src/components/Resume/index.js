c35a0685fb790e22636d43c5f51e4470aa86de12
Patrick Dunn <pmdunn78@gmail.com>
2024-03-05 22:53:52 -0500

    adding scrolling tech icons, need to useRef

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,5 +1,49 @@
 const Resume = () => {
-  return <div>This is the Resume page</div>;
+  return (
+    <div className="resume-page container dev-border">
+      <h2 className="header-md">Resume</h2>
+      <div className="skills-wrapper dev-border">
+        <ul className="skills">
+          <li>
+            <i className="fab fa-github"></i>
+          </li>
+          <li>
+            <i className="fab fa-css3-alt"></i>
+          </li>
+          <li>
+            <i className="fab fa-bootstrap"></i>
+          </li>
+          <li>
+            <i className="fab fa-js-square"></i>
+          </li>
+          <li>
+            <i className="fab fa-npm"></i>
+          </li>
+          <li>
+            <i className="fas fa-database"></i>
+          </li>
+          <li>
+            <i className="fab fa-html5"></i>
+          </li>
+          <li>
+            <i className="fas fa-file-code"></i>
+          </li>
+          <li>
+            <i className="fab fa-codepen"></i>
+          </li>
+          <li>
+            <i className="fab fa-dev"></i>
+          </li>
+          <li>
+            <i className="fab fa-react"></i>
+          </li>
+          <li>
+            <i className="fab fa-node"></i>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
