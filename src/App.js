2b8edcf22c2b262086c9c1bce6c1d5197421db23
Patrick Dunn <pmdunn78@gmail.com>
2024-03-11 23:00:19 -0400

    reset main App component, footer, and styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import Header from "./components/Header";
-import Nav from "./components/Nav";
+// import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import Page from "./components/Page";
-import { useLocation } from "react-router-dom";
 
 const App = () => {
-  const currentPage = useLocation().pathname;
-
   return (
     <div id="page-container">
       <div id="content-wrap">
-        <Header>
-          <Nav currentPage={currentPage} />
-        </Header>
-        <main>
-          <Page currentPage={currentPage} />
-        </main>
+        <Header>{/* <Nav /> */}</Header>
+        <main></main>
       </div>
       <Footer />
     </div>
--- src/index.js
+++ src/index.js
@@ -1,44 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createHashRouter, RouterProvider } from "react-router-dom";
-import About from "./components/About";
-import Portfolio from "./components/Portfolio";
-import Contact from "./components/Contact";
-import Resume from "./components/Resume";
 
 import App from "./App";
 
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-    // errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <About />,
-      },
-      {
-        path: "Portfolio",
-        element: <Portfolio />,
-      },
-      {
-        path: "Contact",
-        element: <Contact />,
-      },
-      {
-        path: "Resume",
-        element: <Resume />,
-      },
-    ],
-  },
-]);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
