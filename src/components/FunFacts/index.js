66e0798af4741287b4363240141b3f7ebc180a75
Patrick Dunn <pmdunn78@gmail.com>
2024-03-17 20:44:53 -0400

    updating text and responsiveness for facts section

--- src/components/FunFacts/index.js
+++ src/components/FunFacts/index.js
@@ -2,18 +2,18 @@ const FunFacts = () => {
   return (
     <div id="facts" className="facts-wrapper">
       <section className="facts container">
-        <div className="facts-segment">
+        <div className="facts-segment   ">
           <h2 className="sub-header">Superpowers</h2>
           <div className="facts-icon">
             <i className="fas fa-code"></i>
           </div>
           <ul>
-            <li>Speak 3 languages: English, Spanish, German</li>
-            <li>Lived in Mexico for 4 years</li>
-            <li>Regularly train Brazilian Jiu Jitsu</li>
+            <li>Communicating technical concepts</li>
+            <li>Learning new frameworks</li>
+            <li>Results-driven development</li>
           </ul>
         </div>
-        <div className="facts-segment">
+        <div className="facts-segment  ">
           <h2 className="sub-header">Fun Facts</h2>
           <div className="facts-icon">
             <i className="fas fa-language"></i>
@@ -24,15 +24,15 @@ const FunFacts = () => {
             <li>Regularly train Brazilian Jiu Jitsu</li>
           </ul>
         </div>
-        <div className="facts-segment">
+        <div className="facts-segment  ">
           <h2 className="sub-header">What matters to me</h2>
           <div className="facts-icon">
             <i className="fas fa-heart"></i>
           </div>
           <ul>
-            <li>Be sincere and honest</li>
-            <li>Practice kindness with yourself and others</li>
-            <li>Appreciate each person's humanity</li>
+            <li>Sincerity and honesty</li>
+            <li>Being kind with yourself and others</li>
+            <li>Appreciating each person's humanity</li>
           </ul>
         </div>
       </section>
