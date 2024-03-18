aa08daed2694a9edc987010cddf54936b8a9e10b
Patrick Dunn <pmdunn78@gmail.com>
2024-03-18 15:31:04 -0400

    profile banner layout and resume link

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,15 +1,34 @@
 const Profile = () => {
   return (
     <div id="profile" className="profile-wrapper">
-      <div className="title">
-        <h2 className="sub-header container">3.5 years in Tech Education</h2>
-        <p>
-          Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum
-          dolor sit amet Lorem ipsum dolor sit amet.
-        </p>
+      <div className="title-bg">
+        <div className="title container">
+          <h2 className="sub-header ">3.5 years in Tech Education</h2>
+          <p>
+            Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum
+            dolor sit amet Lorem ipsum dolor sit amet.
+          </p>
+          <div className="resume-link">
+            <span className="link ">
+              <a
+                href="https://docs.google.com/document/d/1na7m96W9wxRnqFdkmKOGGR1SwPzswAE8vj6YZGe6V0U/edit?usp=sharing"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <div className="icon-text">
+                  <div className="link-icon-wrapper">
+                    <i className="fas fa-lg fa-file-alt"></i>
+                  </div>
+
+                  <span>Resume</span>
+                </div>
+              </a>
+            </span>
+          </div>
+        </div>
       </div>
 
-      <section className="project-cards-wrapper container"></section>
+      <section className="profile-body container"></section>
     </div>
   );
 };
