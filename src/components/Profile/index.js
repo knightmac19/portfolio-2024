6ebe348ad94bcef400d3854a843f5e78e3f55d24
Patrick Dunn <pmdunn78@gmail.com>
2024-03-18 16:52:50 -0400

    adding accomplishments cards

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -28,7 +28,75 @@ const Profile = () => {
         </div>
       </div>
 
-      <section className="profile-body container"></section>
+      <section className="profile-body-wrapper">
+        <div className="profile-body container">
+          <div className="left-items">
+            <div className="card ">
+              <h4>
+                <i className=" icon fas fa-file-signature"></i>Helped students
+                on their journey
+              </h4>
+
+              <ul>
+                <li>Tutored students on coding challenges</li>
+                <li>
+                  Provided technical feedback on thousands of applications
+                </li>
+              </ul>
+            </div>
+            <div className="card ">
+              <h4>
+                <i className=" icon fas fa-language"></i>Led bilingual initiave
+              </h4>
+
+              <ul>
+                <li>Managed group of Spanish-speaking graders</li>
+                <li>Led grading for pilot program given in Spanish</li>
+              </ul>
+            </div>
+            <div className="card ">
+              <h4>
+                <i className=" icon fas fa-microscope"></i>Wrote department SOP
+                on plagiarism
+              </h4>
+
+              <ul>
+                <li>Led virtual training sessions</li>
+                <li>Empowered graders to quickly & efficiently find proof</li>
+                <li>Focused on student growth rather than punishment</li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-items">
+            <div className="card ">
+              <h4>
+                <i className=" icon fas fa-users"></i>Managed team of graders
+              </h4>
+
+              <ul>
+                <li>Led individual & group coaching sessions</li>
+                <li>Ran monthly audits to assess KPI's</li>
+                <li>Conducted performance reviews</li>
+                <li>Designed and implemented Corrective Action Plans</li>
+              </ul>
+            </div>
+            <div className="card ">
+              <h4>
+                <i className="icon fas fa-fire-extinguisher"></i>Proactively
+                addressed emergencies
+              </h4>
+
+              <ul>
+                <li>Preempted issues by auditing graduating classes</li>
+                <li>Recorded training videos for graders</li>
+                <li>
+                  Developed resources explaining new curriculum assignments
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
